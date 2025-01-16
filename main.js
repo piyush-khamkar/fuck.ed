03bcf0d14cc4c766feb0cb2dd31031c3ee57ba79
piyush-khamkar <155073709+piyush-khamkar@users.noreply.github.com>
2025-01-16 23:51:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById('exploreBtn').addEventListener('click', function() {
+    var galaxyContainer = document.getElementById('galaxyContainer');
+    galaxyContainer.style.display = galaxyContainer.style
